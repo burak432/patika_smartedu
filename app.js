b020038c274037d4beb4a0e5430e1eac4772531a
Burak Atilla <burak.atilla@gmail.com>
2023-03-28 17:26:57 +0300

    lesson 02

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 //express
 const express = require("express");
 const app = express();
+const path = require("path");
+
+//static files
+app.use(express.static(path.join(__dirname, "public")));
+
+//template engine - ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //////////////////////routes//////////////////////
 app.get("/", (req, res) => {
-  res.send("hello me");
+  res.status(200).render("index.ejs", {
+    pageName: "index",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about.ejs", {
+    pageName: "about",
+  });
 });
 
 //server listener
