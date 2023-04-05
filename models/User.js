7d15d32b1435ffb6e812e626542f0f3a89b3e4dd
Burak Atilla <burak.atilla@gmail.com>
2023-04-05 22:50:31 +0300

    lesson 08 - added bcrypt for passwords

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -14,6 +15,14 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre("save", function (next) {
+  const user = this;
+  bcrypt.hash(user.password, 10, (error, hash) => {
+    user.password = hash;
+    next();
+  });
 });
 
 const User = mongoose.model("User", UserSchema);
